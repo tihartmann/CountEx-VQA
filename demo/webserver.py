5b9a5c87da6f99b39c00a86ecbabe63d4125ee6e
Timo <hartmanntimo@hotmail.de>
2021-08-14 00:34:30 +0200

    minor fix

--- demo/webserver.py
+++ demo/webserver.py
@@ -31,10 +31,10 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [x for x in request.form.values()]
-    img = features[0]
-    question = features[1]
-    infer(img,question)
+    features = [print(x) for x in request.form.values()]
+    #img = features[0]
+    #question = features[1]
+    #infer(img,question)
 
 
 normalize_img = transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
