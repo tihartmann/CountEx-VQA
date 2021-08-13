70c1187c1d9636930ebe90e773c459a2a22a1e4e
Timo <hartmanntimo@hotmail.de>
2021-08-13 23:35:43 +0200

    fixing dependencies

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,2 +1,6 @@
 import sys
 sys.path.append("../")
+sys.path.append("../VQA/vqa_pytorch/vqa/")
+sys.path.append("../VQA/vqa_pytorch/")
+sys.path.append('../VQA/vqa_pytorch/vqa/external/skip-thoughts.torch/pytorch/')
+sys.path.append('../VQA/vqa_pytorch/vqa/external/pretrained-models.pytorch/')
