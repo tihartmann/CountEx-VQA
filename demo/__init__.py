d7791bfcee987961c7801968e098e5665e89a007
Timo <hartmanntimo@hotmail.de>
2021-08-13 23:36:50 +0200

    minor fix

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append("../")
-sys.path.append("../model/")
 sys.path.append("../VQA/vqa_pytorch/vqa/")
 sys.path.append("../VQA/vqa_pytorch/")
 sys.path.append('../VQA/vqa_pytorch/vqa/external/skip-thoughts.torch/pytorch/')
